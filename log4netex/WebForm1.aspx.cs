d6d9d460a2bf52330a80a42648359cca966a67de
SaiPreethi Induri <Saipreethi_Induri@epam.com>
2020-02-10 10:51:30 +0530

    Add project files.

---
+++ log4netex/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using log4net;
+
+namespace log4netex
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ILog logger = log4net.LogManager.GetLogger("ErrorLog");
+                try
+                {
+                    throw new Exception("Divide By zero exception", new DivideByZeroException());
+
+                }
+                catch (Exception ee)
+                {
+                    logger.Error(ee.Message);
+                }
+            }
+
+        }
+    }
+}
